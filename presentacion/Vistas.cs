fa0281049609063d917f9f02afe2ebd57ec92fe6
erikaTovar <tovarg.erika@gmail.com>
2023-09-24 13:30:56 -0300

    Vista Usuario Vendedor y Usuario Supervisor - Tarea EH-19

--- presentacion/Vistas.cs
+++ presentacion/Vistas.cs
@@ -43,5 +43,44 @@ namespace Presentacion
             Console.WriteLine("| Ver usuarios registrados   |            2             |");
             Console.WriteLine("| Salir                      |     3 o cualquier tecla. |\n");
         }
+
+        public static void VistaSupervisor()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine((" ").PadRight(48, '=') + "   " + DateTime.Now + $"{0:D}" + "   " + (" ").PadRight(48, '='));
+            Console.WriteLine("| Opcion                     | Marcar |");
+            Console.WriteLine("|----------------------------|--------|");
+            Console.WriteLine("| Listar Productos           |    1   |");
+            Console.WriteLine("| Alta de Productos          |    2   |");
+            Console.WriteLine("| Modificacion de Productos  |    3   |");
+            Console.WriteLine("| Baja de Productos          |    4   |");
+            Console.WriteLine("| Devolucion                 |    5   |");
+            Console.WriteLine("| Reportes                   |    6   |");
+            Console.WriteLine("| Salir                      |    7   |\n");
+        }
+
+        public static void VistaVendedor()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine((" ").PadRight(48, '=') + "   " + DateTime.Now + $"{0:D}" + "   " + (" ").PadRight(48, '='));
+            Console.WriteLine("| Opcion                     | Marcar |");
+            Console.WriteLine("|----------------------------|--------|");
+            Console.WriteLine("| Listar Productos           |    1   |");
+            Console.WriteLine("| Registrar Venta Producto   |    2   |");
+            Console.WriteLine("| Reporte de Ventas          |    3   |");
+            Console.WriteLine("| Salir                      |    5   |\n");
+        }
+
+        public static void VistaReportes()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine((" ").PadRight(48, '=') + "   " + DateTime.Now + $"{0:D}" + "   " + (" ").PadRight(48, '='));
+            Console.WriteLine("| Opcion                                          | Marcar |");
+            Console.WriteLine("|-------------------------------------------------|--------|");
+            Console.WriteLine("| Reporte de stock crítico                        |    1   |");
+            Console.WriteLine("| Reporte de Ventas                               |    2   |");
+            Console.WriteLine("| Reporte de productos más vendido por categoría  |    3   |");
+            Console.WriteLine("| Salir                                           |    4   |\n");
+        }
     }
 }
