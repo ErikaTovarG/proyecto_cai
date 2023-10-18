f3cb0993a8973ba3217c4724588b988455e531d6
erikaTovar <tovarg.erika@gmail.com>
2023-10-18 16:13:56 -0300

    Agrego clase ProveedorDatos que conecta con el web services

---
+++ AccesoDatos/ProveedoresDatos.cs
@@ -0,0 +1,30 @@
+﻿using AccesoDatos.Utilidades;
+using Modelo.Proveedor;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    internal class ProveedoresDatos
+    {
+        public static List<Proveedor> ListarProveedores()
+        {
+            HttpResponseMessage response = WebHelper.Get("Proveedor/TraerProveedores");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados");
+            }
+            else
+            {
+                var contentStream = response.Content.ReadAsStringAsync().Result;
+                List<Proveedor> listadoProveedores = JsonConvert.DeserializeObject<List<Proveedor>>(contentStream);
+                return listadoProveedores;
+            }
+        }
+    }
+}
