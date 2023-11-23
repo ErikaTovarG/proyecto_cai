3321735b52349a8dee5eea59f69dac034bf1fcfb
erikaTovar <tovarg.erika@gmail.com>
2023-11-23 01:01:44 -0300

    Avance formulario devolucion de venta

--- AccesoDatos/VentaDatos.cs
+++ AccesoDatos/VentaDatos.cs
@@ -1,4 +1,6 @@
 ﻿using AccesoDatos.Utilidades;
+using Modelo.Cliente_Modelo;
+using Modelo.UsuarioModelo;
 using Modelo.VentaModelo;
 using Newtonsoft.Json;
 
@@ -23,6 +25,38 @@ namespace AccesoDatos
             else
             {
                 Console.WriteLine("Venta agregada exitosamente.");
+            }
+        }
+
+        public static List<VentaWebService> ListarVentasPorCliente(Guid idcliente)
+        {
+            HttpResponseMessage response = WebHelper.Get("Venta/GetVentaByCliente?id=" + idcliente);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("verifique los datos ingresados");
+            }
+            else
+            {
+                var contentstream = response.Content.ReadAsStringAsync().Result;
+                List<VentaWebService> ventas = JsonConvert.DeserializeObject<List<VentaWebService>>(contentstream);
+                return ventas;
+
+            }
+        }
+        public static void DevolucionVenta(string idVenta, string idUsuarioMaster)
+        {
+            Dictionary<String, String> map = new Dictionary<String, String>();
+            map.Add("id", idVenta);
+            map.Add("idUsuario", idUsuarioMaster);
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            HttpResponseMessage response = WebHelper.Patch("Venta/DevolverVenta", jsonRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados");
             }
         }
 
--- negocio/Venta/ClsVenta.cs
+++ negocio/Venta/ClsVenta.cs
@@ -11,6 +11,15 @@ namespace Negocio.Venta
         {
             VentaDatos.CrearVenta(venta);
         }
-
+        public static List<VentaWebService> ListarVentasPorCliente(Guid idCliente)
+        {
+            return VentaDatos.ListarVentasPorCliente(idCliente);
+        }
+        public static void DevolucionVenta(string idVenta)
+        {
+            string usuarioAdministrador = "f2547a8f-d1ee-4508-a53e-846c466beafd";
+            //string usuarioAdministrador = "D347CE99-DB8D-4542-AA97-FC9F3CCE6969";
+            VentaDatos.DevolucionVenta(idVenta, usuarioAdministrador);
+        }
     }
 }
